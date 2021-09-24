75cc211e89f3512cdd66ab4421e937c9f94e40b9
zy <zy@163.com>
2021-09-24 15:44:50 +0800

    first commit

--- Source2Flink2Hbase/src/main/java/Main/Flink2Hbase.java
+++ Source2Flink2Hbase/src/main/java/Main/Flink2Hbase.java
@@ -4,6 +4,8 @@ import entity.Ldcode;
 
 import func.LdcodeMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
@@ -22,13 +24,20 @@ import java.util.Properties;
  */
 public class Flink2Hbase {
     public static void main(String[] args) throws Exception {
+
+//        设置每个taskmanager管理的slot数量，下面设置的是4
+//        final Configuration configuration = new Configuration();
+//        configuration.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, 4);
+//        StreamExecutionEnvironment env =
+//                StreamExecutionEnvironment.getExecutionEnvironment(configuration);
+
+
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
-
         Properties prop = new Properties();
         prop.put("bootstrap.servers", "zyVM:9092");
         prop.put("kafka.zookeeper.connect", "zyVM:2181");
-        prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         Connection connection; // 管理Hbase连接
 
         DataStreamSource<String> ldcodeSource = env.addSource(new FlinkKafkaConsumer<String>("ldcode", new SimpleStringSchema(), prop));
